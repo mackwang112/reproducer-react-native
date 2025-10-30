ffa369a2f2d70864f08ee300804e9fd8a3e56000
mackwang112 <mackwang112@gmail.com>
2025-10-30 17:10:03 +0800

    init

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -5,41 +5,19 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+let count = 0;
 
+const App = () => {
+  console.log('render App...................' + ++count)
+  // throw new Error("Test Error: count=" + count);
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>React Native Project 77</Text>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+  )
+};
 
 export default App;
